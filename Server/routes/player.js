e7c321eb4c2e97392e07b805ece5e73dc5081f55
Kaustubh Lohani <kaustubhlohani25@gmail.com>
2023-05-05 09:28:02 -0400

    player stats

--- Server/routes/player.js
+++ Server/routes/player.js
@@ -8,6 +8,48 @@ const redis = require("redis");
 const client = redis.createClient();
 client.connect().then(() => {});
 
+function formattedDate(dateString) {
+  if (dateString) {
+    const date = new Date(dateString);
+    const today = new Date();
+    const diffInMs = today.getTime() - date.getTime();
+    const diffInYears = Math.floor(diffInMs / (1000 * 60 * 60 * 24 * 365.25));
+    const month = date.toLocaleString("default", { month: "short" });
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const outputString = `${month} ${day}, ${year} (${diffInYears} years)`;
+    return outputString;
+  } else {
+    return "";
+  }
+}
+
+function getStats(stats) {
+  let battingStats = stats?.filter((x) => x.fn == "batting");
+  let bowlingStats = stats?.filter((x) => x.fn == "bowling");
+  let matchtypes = ["odi", "t20i", "ipl"];
+  let newStats = {
+    batting: matchtypes.map((matchtype) => {
+      let obj = { matchtype };
+      let arr = battingStats?.filter((x) => x.matchtype == matchtype);
+      arr?.forEach((element) => {
+        obj[element.stat] = element.value;
+      });
+      return obj;
+    }),
+    bowling: matchtypes.map((matchtype) => {
+      let obj = { matchtype };
+      let arr = bowlingStats?.filter((x) => x.matchtype == matchtype);
+      arr?.forEach((element) => {
+        obj[element.stat] = element.value;
+      });
+      return obj;
+    }),
+  };
+
+  return newStats;
+}
+
 router.get("/playersList/page/:pageNo", async (req, res) => {
   try {
     let pageNo = req.params.pageNo;
@@ -15,11 +57,15 @@ router.get("/playersList/page/:pageNo", async (req, res) => {
     console.log(validationStatus);
     if (!validationStatus.isValid) {
       if (validationStatus.message.includes("404")) {
-        res.status(404).json({ errorCode: 404, message: validationStatus.message });
+        res
+          .status(404)
+          .json({ errorCode: 404, message: validationStatus.message });
         return;
       }
       if (validationStatus.message.includes("400")) {
-        res.status(400).json({ errorCode: 400, message: validationStatus.message });
+        res
+          .status(400)
+          .json({ errorCode: 400, message: validationStatus.message });
         return;
       }
     }
@@ -52,7 +98,9 @@ router.get("/search/:searchTerm", async (req, res) => {
   try {
     let searchTerm = req.params.searchTerm;
     if (!client.isOpen) await client.connect();
-    let searchedPlayersListFromCache = await client.get("searchPlayer_" + searchTerm);
+    let searchedPlayersListFromCache = await client.get(
+      "searchPlayer_" + searchTerm
+    );
     if (searchedPlayersListFromCache) {
       console.log("searched players from redis");
       res.status(200).json(JSON.parse(searchedPlayersListFromCache));
@@ -60,7 +108,10 @@ router.get("/search/:searchTerm", async (req, res) => {
     } else {
       let searchedPlayersList = await players.searchPlayersByName(searchTerm);
       try {
-        await client.set("searchPlayer_" + searchTerm, JSON.stringify(searchedPlayersList));
+        await client.set(
+          "searchPlayer_" + searchTerm,
+          JSON.stringify(searchedPlayersList)
+        );
       } catch (e) {
         console.log("Set searched playersList in Redis Error");
         console.log(e);
@@ -80,13 +131,17 @@ router.get("/player/:id", async (req, res) => {
   try {
     let id = req.params.id;
     if (!client.isOpen) await client.connect();
+    
     let playerFromCache = await client.get("player_" + id);
+    
     if (playerFromCache) {
       console.log("player from redis");
       res.status(200).json(JSON.parse(playerFromCache));
       return;
     } else {
       let playerObj = await players.getPlayerById(id);
+      playerObj.dateOfBirth = formattedDate(playerObj.dateOfBirth)
+      playerObj.stats = getStats(playerObj.stats)
       try {
         await client.set("player_" + id, JSON.stringify(playerObj));
       } catch (e) {
